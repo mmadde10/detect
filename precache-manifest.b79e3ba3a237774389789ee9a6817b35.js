d9c0463ea5cbaea1d3d0fcd55411b5df8d9cac6a
mmadde10 <mmadden1495@gmail.co>
2020-08-13 15:54:29 -0500

    Updates

---
+++ precache-manifest.b79e3ba3a237774389789ee9a6817b35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e066d26ab026e82eb99d0c52b99a72c3",
+    "url": "/detect/index.html"
+  },
+  {
+    "revision": "6da04dbb8640cc988e2e",
+    "url": "/detect/static/css/main.01729944.chunk.css"
+  },
+  {
+    "revision": "5eba503ff191a7e172e7",
+    "url": "/detect/static/js/2.b9f1fa2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/detect/static/js/2.b9f1fa2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da04dbb8640cc988e2e",
+    "url": "/detect/static/js/main.8010382d.chunk.js"
+  },
+  {
+    "revision": "167cd7321939e4530ef2",
+    "url": "/detect/static/js/runtime-main.b3cb7375.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/detect/static/media/logo.5d5d9eef.svg"
+  }
+]);
